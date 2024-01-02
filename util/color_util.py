960774be97a549f37ed7e2cd185ac17e36c0b6e3
maheshJosephSadashiv <mahesh.sadashiv@gmail.com>
2024-01-02 00:48:54 -0800

    reflection effect

--- util/color_util.py
+++ util/color_util.py
@@ -9,4 +9,16 @@ def intensity_multiplier(color, I):
     if green > 255:
         green = 255
     return (red, green, blue)
+
+def color_add(color1, color2):
+    red = color1[0]+color2[0]
+    green = color1[1]+color2[1]
+    blue = color1[2]+color2[2]
+    if red > 255:
+        red = 255
+    if blue > 255:
+        blue = 255
+    if green > 255:
+        green = 255
+    return (red, green, blue)
         
